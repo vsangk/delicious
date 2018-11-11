10e04bbbc1b583ad520c81c10a88b482eb52fb41
Vince Sangkhae <vsangk@gmail.com>
2018-11-10 20:12:10 -0800

    Add store controller and model

---
+++ models/Store.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise; // set mongoose promise to use native promise
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!',
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+});
+
+// normal function scope is important here because we want the store
+// scope of this instead of the outer scope
+storeSchema.pre('save', function(next) {
+  if (!this.isModified('name')) {
+    next(); //skip it
+    return; // stop this function from running
+  }
+
+  this.slug = slug(this.name);
+  next();
+});
+
+module.exports = mongoose.model('Store', storeSchema);
