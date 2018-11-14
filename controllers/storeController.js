e2e26f076d1060aca0caed9357d30de3f6211bfe
Vince Sangkhae <vsangk@gmail.com>
2018-11-13 20:35:51 -0800

    Allow for creating and editing stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +9,39 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await new Store(req.body).save();
+  await store.save();
+  req.flash(
+    'success',
+    `Successfully created ${store.name}. Care to leave a review?`
+  );
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
+
+exports.editStore = async (req, res) => {
+  // 1. Find the store
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confirm they are the owner of the store
+  // 3. Render outo the edit form so the user can update their stores
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true, // return the new store instead of the old one
+    runValidators: true, // validators usually only run on create, need this when updating
+  }).exec();
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="stores/${
+      store.slug
+    }">View Store -> </a>`
+  );
+  res.redirect(`/store/${store._id}/edit`);
 };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', storeController.homePage);
+router.get('/stores', storeController.getStores);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add/:id', storeController.updateStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.get('/store/:id/edit', storeController.editStore);
 
 module.exports = router;
